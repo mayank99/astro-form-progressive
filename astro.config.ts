2749658fe4ed9baece97a7eca99927c078c98e57
Mayank <mayank99@users.noreply.github.com>
2022-08-24 22:29:25 -0400

    Install solid

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
-	output: 'server',
+  adapter: vercel(),
+  output: 'server',
+  integrations: [solidJs()]
 });
